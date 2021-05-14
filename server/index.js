007f5d6083d3965652abd8bdf5b5ad67a982158c
Sue Ann Kim <kimsueann@gmail.com>
2021-05-14 13:54:34 -0700

    Changed loaderio verfication for port 4242

--- server/index.js
+++ server/index.js
@@ -3,47 +3,12 @@ const cors = require('cors');
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
-const { Pool, Client, Query } = require('pg');
-const copyFrom = require('pg-copy-streams').from;
-// const productRoutes = require('./productRoutes.js');
+const { Client, Query } = require('pg');
 const router = require('express').Router();
-const csv = require('csv-parser');
-
-const tables = [
-  {
-    table : 'product',
-    path : path.join(__dirname, '../db_products/product.csv'),
-    cmd: `COPY product FROM STDIN WITH (FORMAT CSV, HEADER)`
-  },
-  {
-    table: 'styles',
-    path: path.join(__dirname, '../db_products/styles.csv'),
-    cmd: `COPY styles FROM STDIN DELIMITER ',' CSV HEADER`
-  },
-  {
-    table: 'sku',
-    path: path.join(__dirname, '../db_products/skus.csv'),
-    cmd: `COPY sku FROM STDIN DELIMITER ',' CSV HEADER`
-  },
-  {
-    table: 'photos',
-    path: path.join(__dirname, '../db_products/photos.csv'),
-    cmd: `COPY photos FROM STDIN DELIMITER ',' CSV HEADER`
-  },
-  {
-    table: 'feature',
-    path: path.join(__dirname, '../db_products/features.csv'),
-    cmd: `COPY feature FROM STDIN WITH (FORMAT CSV)`
-  },
-  {
-    table: 'related',
-    path: path.join(__dirname, '../db_products/related.csv'),
-    cmd: `COPY related FROM STDIN DELIMITER ',' CSV HEADER`
-  }
-];
+//This is Products Service don't MERGE with MASTER
 
 const app = express();
-const PORT = 3030;
+const PORT = 4242;
 
 app.use(express.json());
 app.use(cors());
@@ -68,67 +33,6 @@ client.connect(err => {
     console.log('connected to Database!')
   }
 });
-
-const createIndex = [
-  {name: 'styles', col: 'product_id'},
-  {name: 'sku', col: 'style_id'},
-  {name: 'photos', col: 'style_id'},
-  {name: 'feature', col: 'product_id'},
-  {name: 'related', col: 'current'}
-];
-
-const executeQuery = (tables) => {
-  const execute = (target, callback) => {
-    client.query(`Truncate feature`, (err) => {
-      if (err) {
-        client.end();
-        console.log('error truncating table', err);
-        callback(err)
-      } else {
-        console.log('Truncated');
-        callback(null, target)
-      }
-    });
-  }
-  tables.forEach((entry, index) => {
-    execute(entry.table, (err) => {
-      if(err) console.log(err);
-
-      const fileStream = fs.createReadStream(`${entry.path}`);
-      const copyStream = client.query(copyFrom(`${entry.cmd}`));
-
-      console.time(`${entry.table}`); // console time
-      console.time(`copy ${entry.table}`)
-
-      fileStream.pipe(copyStream);
-
-      fileStream.on('error', (error) => {
-        console.log(`Error reading file: ${entry.table}`);
-      })
-      copyStream.on('error', (error) => {
-        console.log(`Error during copying: ${error}`);
-      })
-      fileStream.on('end', () => {
-        console.log(`Completed reading stream ${entry.table}`);
-        console.timeEnd(`${entry.table}`); //console time
-        console.timeEnd(`copy ${entry.table}`);
-
-        let tableNameInd = createIndex[index + 1].name;
-        let colNameInd = createIndex[index + 1].col;
-
-        if (`${entry.table}` === tableNameInd) {
-          client.query(`CREATE INDEX idx_${entry.table}_${colNameInd} ON ${entry.table}(${colNameInd})`, (err, result) => {
-            if(err) console.log(err);
-            console.log(`created product index at ${entry.table}:`, result);
-          })
-        }
-
-      })
-    })
-  })
-}
-
-// executeQuery(tables);
 
 app.get('/products', (req, response) => {
   let page = req.query.page || 1;
@@ -194,8 +98,8 @@ app.get('/products/:product_id/related', (req, res) => {
   })
 });
 
-app.get('/loaderio-a3f5e03fd5bfbdc801e89ce77236e5b7/', (req, res) => {
-  res.send('loaderio-a3f5e03fd5bfbdc801e89ce77236e5b7');
+app.get('/loaderio-d386834c0339b7c4ffe3f2c6c9a33d95.txt', (req, res) => {
+  res.send('loaderio-d386834c0339b7c4ffe3f2c6c9a33d95');
 })
 
 // router.route('/cart')
